ec87e31f0ac7c7705084a81d29b89503a8dbc9b4
Marcelo Delgado <marceloedelgado@gmail.com>
2021-05-30 17:56:10 -0300

    Actualizacion del archivo apps.js con codigo de React

--- apps.js
+++ apps.js
@@ -1,1 +1,30 @@
+import React from 'react'
 console.log("Hola Mundo, me falto este mensaje");
+const task = [
+{ id: 1, name: "Tarea 1", completed: false, assigned: "Carlos" },
+{id: 2, name: "Tarea 2", completed: false, assigned: "Bob"},
+{id: 3, name: "Tarea 3", completed: false, assigned: "Zack"}
+];
+
+function TodoList () {
+    const deleteTask = () => {
+        //TODO: Delete task;
+    } 
+
+    const updateTask = () => {
+        //TODO: Delete task;
+    }
+
+    for (let task of tasks) {
+        //TODO: Loop trough tasks and delete.
+    }
+    return (
+        <ul>
+            {task.map(t => (
+                <li key={t.id}>{t.name} <button onClick={}></button>  </li>))}
+        </ul>
+    )
+}
+
+
+ 
